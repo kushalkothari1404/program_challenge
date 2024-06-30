0378a5b263d1f2807fdf8164c0dcc41af3210f9b
kushalkothari1404 <168670419+kushalkothari1404@users.noreply.github.com>
2024-06-30 13:55:03 +0530

    Create sum.cpp

---
+++ sum.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int total(int n)
+{
+    int sum = 0;
+    int i;
+    if(n>0)
+    {
+        for(i=1;i<=n;i++)
+        {
+            sum+=i;
+        }
+    }
+    else if(n<0)
+    {
+        for(i=n;i=1;i++)
+        {
+            sum+=i;
+        }
+    }
+    return sum;
+}
+int main()
+{
+    int n;
+    cout<<"Enter the number of terms:";
+    cin>>n;
+    cout<<"The Sum of "<<n<<" number(s) is: "<<total(n)<<endl;
+    return 0;
+}
